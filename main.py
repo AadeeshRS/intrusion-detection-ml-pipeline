0c7c7aab1f4a05bc1fa8aa72c240cbbed091e49b
AadeeshRS <srivastavaaadeesh@gmail.com>
2026-02-16 19:36:40 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -3,25 +3,35 @@ from src.components.data_validation import DataValidation
 from src.components.data_validation import DataValidationConfig
 from src.exception.exception import NetworkSecurityException
 from src.logging.logger import logging
-from src.entity.config_entity import DataIngestionConfig
-from src.entity.config_entity import TrainingPipelineConfig
+from src.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataTransformationConfig
 import sys
+from src.components.data_transformation import DataTransformation
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
-        trainingpipelineconfig=TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(training_pipeline_config=trainingpipelineconfig)
-        data_ingestion=DataIngestion(dataingestionconfig)
-        datavalidationconfig=DataValidationConfig(training_pipeline_config=trainingpipelineconfig)
+        trainingpipelineconfig = TrainingPipelineConfig()
+        dataingestionconfig = DataIngestionConfig(
+            training_pipeline_config=trainingpipelineconfig
+        )
+        data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Initiate the data Ingestion")
-        dataingstionartifact=data_ingestion.initate_data_ingestion()
+        dataingstionartifact = data_ingestion.initate_data_ingestion()
         logging.info("Data Initiation completed")
         print(dataingstionartifact)
+        datavalidationconfig = DataValidationConfig(
+            training_pipeline_config=trainingpipelineconfig
+        )
         data_validation = DataValidation(dataingstionartifact, datavalidationconfig)
         logging.info("Initiate the data validation")
-        data_validation.initiate_data_validation()
+        data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data validation completed")
-        print(datavalidationconfig)
+        print(data_validation_artifact)
+        logging.info("Initiate the data Transformation")
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation completed")
+        print(data_transformation_artifact)
 
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e, sys)
--- src/constants/training_pipeline/__init__.py
+++ src/constants/training_pipeline/__init__.py
@@ -1,25 +1,36 @@
-import os,sys
+import os
+import sys
 import pandas as pd
 import numpy as np
 
-TARGET_COLUMNS="Result"
-PIPELINE_NAME:str="NetworkSecurity"
-ARTIFACT_DIR:str="Artifacts"
-FILE_NAME:str="phisingData.csv"
-TRAIN_FILE_NAME:str="train.csv"
-TEST_FILE_NAME:str="test.csv"
+TARGET_COLUMNS = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "phisingData.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
 
-SCHEMA_FILE_PATH=os.path.join('data_schema',"schema.yaml")
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
-DATA_INGESTION_COLLECTION_NAME:str="NetworkData"
-DATA_INGESTION_DATABASE_NAME:str="AADEESHAI"
-DATA_INGESTION_DIR_NAME:str="data_ingestion"
-DATA_INGESTION_FEATURE_STORE_DIR:str="feature_store"
-DATA_INGESTION_INGESTED_DIR:str="ingested"
-DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float=0.2
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
+DATA_INGESTION_DATABASE_NAME: str = "AADEESHAI"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
-DATA_VALIDATION_DIR_NAME:str="data_validation"
-DATA_VALIDATION_VALID_SIR:str="validated"
-DATA_VALIDATION_INAVLID_DIR:str="invalid"
-DATA_VALIDATION_DRFIT_REPORT_DIR:str="drift_report"
-DATA_VALIDATION_DRFIT_REPORT_FILE_NAME:str="report.yaml"
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_SIR: str = "validated"
+DATA_VALIDATION_INAVLID_DIR: str = "invalid"
+DATA_VALIDATION_DRFIT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRFIT_REPORT_FILE_NAME: str = "report.yaml"
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
+DATA_TRANSFORMATION_IMPUTER_PARAMS:dict={
+        "missing_values":np.nan,
+        "n_neighbors":3,
+        "weights":"uniform"
+    }
